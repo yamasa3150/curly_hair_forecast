8fd8562388b253baa755cf2c962ccbc10449d653
yamasa3150 <itnzsc4b@icloud.com>
2021-07-29 11:25:05 +0900

    Add:仮のtopページ作成とjquery追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'top#index'
   post '/callback' => 'line_bot#callback'
+  resources :push_settings
 end
