44d21199111a7fa6a7a6bce170246b6df6ec5bb1
yamasa3150 <itnzsc4b@icloud.com>
2021-10-21 10:53:47 +0900

    delete:prefecture_idカラム消去

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,3 @@
 class UsersController < ApplicationController
-  def edit
-  end
-
-  def update
-  end
-
-  private 
-
-  def user_params
-    params.require(:user).permit(:line_user_id, :prefecture_id)
-  end
+  
 end
